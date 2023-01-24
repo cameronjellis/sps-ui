29e880c985ddb265b30427e729f2e7abe9ea4646
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-24 13:09:50 -0700

    initial commit

---
+++ src/components/FieldEditor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FieldEditor = () => {
+  return (
+    <div>
+      <h1>fieldeditor</h1>
+    </div>
+  );
+};
+
+export default FieldEditor;
---
+++ src/root.component.js
@@ -0,0 +1,9 @@
+import FieldEditor from "./components/FieldEditor";
+
+export default function Root(props) {
+  return (
+    <div>
+      <section>{props.name} is mounted!</section>;
+    </div>
+  );
+}
