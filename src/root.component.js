c5109ad6848350ba5dba791caa4f94c4e11b8dac
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-30 15:14:48 -0700

    style

--- src/root.component.js
+++ src/root.component.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EditPage from "./pages/EditPage";
 import MicroserviceList from "./pages/MicroserviceList";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 export default function Root() {
   //react router here -> rendering top level components/pages based on nav app
